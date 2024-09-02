2d4ac00a9185569d0ac9862abdacdee892c83f6e
Roberto <rgalanllorca@me.com>
2024-09-02 21:06:51 +0200

    Start testing part

---
+++ tests/api/calendarApi.test.js
@@ -0,0 +1,19 @@
+import calendarApi from "../../src/api/calendarApi"
+
+describe('Pruebas en el CalendarApi', () => {
+    
+    test('debe tener la configuración por defecto', () => {
+
+        expect( calendarApi.defaults.baseURL ).toBe( process.env.VITE_API_URL );
+        
+    });
+
+    test('debe tener el x-token en el header de todas las peticiones', async() => {
+        
+        const token = 'ABC-123-XYZ';
+        localStorage.setItem('token', token);
+        const res = await calendarApi.get('/auth');
+
+        expect( res.config.headers['x-token'] ).toBe( token );
+    });
+})
